4faa43634fba4a65f18dffc6a95a9a6933b3b93a
david <dzg00@iesemilidarder.com>
2021-04-23 18:54:36 +0200

    1

---
+++ llm 5.3 REFORÇ/js/app.js
@@ -0,0 +1,42 @@
+container = document.getElementById("tiendaContainer");
+
+init();
+
+function init(){
+    cogerRopa();
+}
+
+function cogerRopa(){
+    fetch("./data/tenda.xml")
+                .then(response => response.text())
+                //.then(texto => (new window.DOMParser()).parseFromString(texto, "text/xml"))
+                .then(texto => {
+                    let parser = new window.DOMParser();
+                    return parser.parseFromString(texto, "text/xml");
+                })
+                .then(data => {
+                    const listaRopa = data.querySelectorAll("producto");
+                    listaRopa.forEach(producto => {
+                        let nombreProducto = producto.querySelector("nombreProducto").textContent;
+                        let precioAct = producto.querySelector("precioInicial").textContent;
+                        let precioDesp = producto.querySelector("precioDescuento").textContent;
+                        let img = producto.querySelector("foto").textContent;
+                        container.innerHTML +=`
+                        <div class="card" style="width: 18rem;">
+                        <img src="${img}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">${nombreProducto}</h5>
+                          <p class="card-text">${precioAct}</p>
+                          <p class="card-text">${precioDesp}</p>
+                          <a href="#" class="btn btn-primary">Compralo!</a>
+                        </div>
+                      </div>
+                        `;
+                    });
+                   
+                })
+                .catch(Error => {
+                    alert("Archivo no encontrado")
+                    console.log(Error)
+                });
+        }
